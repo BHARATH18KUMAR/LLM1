b017d3725bc1f53b5889ee00fddd79cb63fabf71
BHARATH18KUMAR <bharath011005@gmail.com>
2026-05-19 11:40:39 +0530

    PIPELINE FILES

--- main.py
+++
@@ -1,8 +0,0 @@
-from LLM_text_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
-try:
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-
-except Exception as e:
-    raise e
